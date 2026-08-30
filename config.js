a61ca50229857a195f9cbb1bef80af0c27a81340
shylth65 <shylth@naver.com>
2026-08-30 21:26:18 +0900

    Load detail action UX v5.19

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ try {
 } catch (_) {}
 window.addEventListener("DOMContentLoaded", () => {
   [["link","v59.css?v=5.10"],["link","v512.css?v=5.12"],["link","v514.css?v=5.17"]].forEach(([t,u])=>{const e=document.createElement(t);e.rel="stylesheet";e.href=u;document.head.appendChild(e)});
-  ["v58.js?v=5.8","v59.js?v=5.10","v511.js?v=5.11","v512.js?v=5.12","v513.js?v=5.13","v514.js?v=5.15","v516.js?v=5.16","v518.js?v=5.18"].forEach(u=>{const s=document.createElement("script");s.src=u;s.defer=true;document.body.appendChild(s)});
+  ["v58.js?v=5.8","v59.js?v=5.10","v511.js?v=5.11","v512.js?v=5.12","v513.js?v=5.13","v514.js?v=5.15","v516.js?v=5.16","v518.js?v=5.18","v519.js?v=5.19"].forEach(u=>{const s=document.createElement("script");s.src=u;s.defer=true;document.body.appendChild(s)});
 });
